0170fe87d9e08be576c19a9689054bb259ffa309
cpustejovsky <charles.pustejovsky@gmail.com>
2021-03-14 18:17:22 -0400

    Update main with session data and use env vars for db pw and sesson secret

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/cpustejovsky/furry-dollop/models"
 	"github.com/cpustejovsky/furry-dollop/models/psql"
+	"github.com/golangcollege/sessions"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -23,11 +25,17 @@ type Config struct {
 	Addr string
 }
 
+type contextKey string
+
+const contextKeyIsAuthenticated = contextKey("isAuthenticated")
+
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	session  *sessions.Session
 	users    interface {
-		Insert(string, string, string) error
+		Authenticate(string, string) (string, error)
+		Insert(string, string, string, string) error
 		Get(string) (*models.User, error)
 		Update(string, string, string, string) (*models.User, error)
 		Delete(string) error
@@ -51,12 +59,12 @@ const (
 )
 
 func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-
-	errorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.LUTC|log.Llongfile)
 	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC)
+	errorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.LUTC|log.Llongfile)
+
+	if err := godotenv.Load(); err != nil {
+		errorLog.Println("No .env file found")
+	}
 }
 
 func main() {
@@ -64,6 +72,10 @@ func main() {
 	cfg := new(Config)
 	flag.StringVar(&cfg.Addr, "addr", ":5000", "HTTP network address")
 	flag.Parse()
+
+	// Environemntal Variables
+	var password = os.Getenv("TEST_PSQL_PW")
+	var sessionSecret = []byte(os.Getenv("SESSION_SECRET"))
 
 	// DB Setup
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
@@ -80,9 +92,16 @@ func main() {
 	}
 	infoLog.Println("Successfully connected to database!")
 
+	// Session Setup
+	session := sessions.New(sessionSecret)
+	session.Lifetime = 12 * time.Hour
+
 	app := &application{
-		users: &psql.UserModel{DB: db},
-		posts: &psql.PostModel{DB: db},
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		session:  session,
+		users:    &psql.UserModel{DB: db},
+		posts:    &psql.PostModel{DB: db},
 	}
 
 	server := &http.Server{
